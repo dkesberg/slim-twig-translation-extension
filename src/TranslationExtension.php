2a4f137bc9f0267995cfed297018cac3cdec8a70
Daniel Kesberg <dkesberg@gmail.com>
2014-07-17 15:46:06 +0200

    typos

--- src/TranslationExtension.php
+++ src/TranslationExtension.php
@@ -1,21 +1,43 @@
 <?php
-
+/**
+ * TranslationExtension adds a translate function for twig templates in a slim framework application.
+ *
+ * TranslationExtension expects an instance of a translator class injected into the slim app and tries 
+ * to call the "trans()" function on the translator class.
+ *
+ * Example usage:
+ * {{ translate('male') }}
+ * {{ _('male') }}
+ * 
+ * @author Daniel Kesberg <dkesberg@gmail.com>
+ * @copyright 2014 Daniel Kesberg
+ * @package Dkesberg\Slim\Twig\Extension
+ * 
+ * MIT LICENSE
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 namespace Dkesberg\Slim\Twig\Extension;
 
 use Slim\Slim;
 
-/**
- * Class TranslatorExtension 
- * TranslatorExtension adds a translate function for twig templates in a slim framework application.
- * 
- * TranslatorExtension expects an instance of a translator class injected into the slim app and tries to call
- * 
- * Example usage:
- * {{ translate('male') }} 
- * {{ _('male') }}
- * 
- * @package     Application\Helper\Twig
- */
 class TranslationExtension extends \Twig_Extension
 {
   public function getName()
@@ -38,7 +60,7 @@ class TranslationExtension extends \Twig_Extension
       throw new \Exception('No translator class found.');
     }
     if (!method_exists($slim->translator, 'trans')) {
-      throw new \Exception('No trans method found in translator class.');
+      throw new \Exception('No translate method found in translator class.');
     }    
     return $slim->translator->trans($name);
   }
